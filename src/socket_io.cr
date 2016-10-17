ae4ad21f9d64f0bd012dff248119ec1946ff3342
forsaken <alexey2142@mail.ru>
2016-10-17 23:32:37 +1000

    update to 0.3.0

--- src/socket_io.cr
+++ src/socket_io.cr
@@ -2,7 +2,7 @@ require "./socket_io/*"
 
 module SocketIO
   class WebSocket
-    def initialize(@socket : HTTP::WebSocket)
+    def initialize(@socket : HTTP::WebSocket, @handler : SocketIO::Base)
       @actions = {} of String => JSON::Any -> Void
       @socket.on_message do |data|
         name, message = parse_data(data)
@@ -13,12 +13,18 @@ module SocketIO
     def on(name : String, &action : JSON::Any -> Void)
       if name == "disconnect"
         @socket.on_close do |data|
-          name, message = parse_data(data)
-          action.call message
+          @handler.sockets.delete self
+          name, message = parse_data(data) unless data != "?"
+          action.call message || JSON::Any.new("")
         end
       else
         @actions[name.to_s] = action
       end
+    end
+
+    def emit(name : String | Symbol, message : NamedTuple)
+      json_message = { name: name, message: message }
+      @socket.send json_message.to_json.to_s
     end
 
     private def parse_data(data : String) : Array(JSON::Any)
@@ -28,20 +34,22 @@ module SocketIO
   end
 
   class Base
+    getter :sockets
+    
     def initialize
-      @sockets = [] of HTTP::WebSocket
+      @sockets = [] of WebSocket
     end
 
     def on_connection(&action : WebSocket -> Void) : HTTP::WebSocketHandler
       HTTP::WebSocketHandler.new do |session|
-        @sockets << session
-        action.call WebSocket.new(session)
+        socket = WebSocket.new(session, self)
+        @sockets << socket
+        action.call socket
       end
     end
 
-    def emit(name : String | Symbol, message : Hash)
-      json_message = { name: name, message: message }
-      @sockets.each { |socket| socket.send json_message.to_json.to_s }
+    def emit(name : String | Symbol, message : NamedTuple)
+      @sockets.each { |socket| socket.emit name, message }
     end
   end
 end
--- src/socket_io/version.cr
+++ src/socket_io/version.cr
@@ -1,3 +1,3 @@
 module SocketIO
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 end
